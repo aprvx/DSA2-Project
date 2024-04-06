752c0a000b1b96d7be68c7f31f7b40c9320f9c82
aprvx <apurvaygokhale@gmail.com>
2024-04-06 23:31:03 +0530

    hashing added with support functions

---
+++ lib/hashing.h
@@ -0,0 +1,13 @@
+#ifndef HASHING_H
+#define HASHING_H
+
+unsigned int hash(char *str);
+void delete_hash_table();
+int insert_hash(char *str);
+int contains(char *str);
+void create_trie(trie *t);
+
+
+
+
+#endif
